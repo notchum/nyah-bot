0751d9e5088636bceb2425547f7daaeb14cbba71
notchum <jmrc98@gmail.com>
2024-11-14 18:16:06 -0700

    add claim refactor migration script

---
+++ models/migrations/20241022125404_claim-refactor-traits.py
@@ -0,0 +1,43 @@
+import os
+import uuid
+from enum import Enum
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+print(f"Database URI: {os.getenv('DATABASE_URI')}")
+client = MongoClient(os.getenv("DATABASE_URI"))
+print("Connected to MongoDB Atlas!")
+if os.getenv("TEST_MODE"):
+    db = client['_waifus']
+else:
+    db = client['waifus']
+claims = db['claims']
+print(f"Collection name: {db.name}.{claims.name}")
+
+
+result = claims.update_many(
+    {},
+    {
+        "$unset": {
+            "price": "",
+            "attack_mod": "",
+            "defense_mod": "",
+            "health_mod": "",
+            "speed_mod": "",
+            "magic_mod": "",
+            "trait_common": "",
+            "trait_uncommon": "",
+            "trait_rare": "",
+            "trait_legendary": "",
+        },
+        "$set": {
+            "trait": 0,
+            "health_points": 0,
+        }
+    }
+)
+
+print(result)
